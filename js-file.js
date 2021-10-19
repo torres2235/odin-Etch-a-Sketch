25ad21590bea678568699db42e4387903460b299
Torres2235 <Torres2235@gmail.com>
2021-10-18 20:13:46 -0700

    adding functionality to change square colors to either black or a random color

--- js-file.js
+++ js-file.js
@@ -1,7 +1,7 @@
 document.body.onload = addElement;
 
+//adds our divs into our grid
 function addElement() {
-
     for(i=0; i < 256; i++) {
         let newDiv = document.createElement('div');
         newDiv.className = "square";
@@ -12,7 +12,36 @@ function addElement() {
     }
 }
 
+//changes background color of the squares in our div
+let currentColor = "";
+let buttonSelect = 0;
 let square = document.getElementById('container');
 square.addEventListener("mouseover", function(e) {
-    e.target.style.backgroundColor = "black";
+    if(buttonSelect == 0) { //change to black
+        e.target.style.backgroundColor = "black";
+    } else if (buttonSelect == 1) { //pick random color
+        e.target.style.backgroundColor = getColor();
+    }
+    
 });
+
+//these two functions toggle if the the grids change to black or a random color
+let blackButton = document.getElementById('blackButton');
+blackButton.addEventListener("click", function(e){
+    return buttonSelect = 0;
+});
+
+let colorButton = document.getElementById('colorButton');
+colorButton.addEventListener("click", function(e){
+    return buttonSelect = 1;
+});
+//------------------------------------------------------------------------------
+
+//these two fucntions generate a random RGB value ("rgb(#, #, #)")
+function getRandomColor() { //To give me a new rgb number everytime
+    return (Math.floor(Math.random() * (255 - 10)) + 10);
+}
+function getColor() {
+  return `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
+}
+//-----------------------------------------------------------------
